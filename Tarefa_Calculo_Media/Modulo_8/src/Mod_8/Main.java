0d41d004132d11da4e724c88fe92cd5f7c25b596
RenanBT <renan.bete@gmail.com>
2022-10-17 19:34:46 -0300

    commit tarefa modulo 8 (calculo da media aritmetica de 4 notas)

---
+++ Tarefa_Calculo_Media/Modulo_8/src/Mod_8/Main.java
@@ -0,0 +1,18 @@
+package Mod_8;
+
+public class Main {
+    public static void main(String[] args) {
+        calculoMedia();
+    }
+
+    public static void calculoMedia (){
+
+        double nota1 = 5.5;
+        double nota2 = 7;
+        double nota3 = 8.5;
+        double nota4 = 10;
+
+        double mediaAritmetica = (nota1 + nota2 + nota3 + nota4)/4;
+                System.out.println(mediaAritmetica);
+    }
+}
