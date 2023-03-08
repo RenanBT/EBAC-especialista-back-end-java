a8cd3ac52be25f0c4864877e091417c177cc8e28
RenanBT <renan.bete@gmail.com>
2023-03-07 21:29:04 -0300

    Tarefa Abstract Factory

---
+++ Swing/CadastroClienteSwing/src/cadastroclienteswing/CadastroClienteSwing.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cadastroclienteswing;
+
+/**
+ *
+ * @author Renan
+ */
+public class CadastroClienteSwing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaPrincipal tela = new TelaPrincipal();
+        tela.setVisible(true);
+                
+    }
+    
+}
+
+/*int result = JOptionPane.showConfirmDialog(this,"Deseja sair da aplicação?", "Sair",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE);
+            if(result == JOptionPane.YES_OPTION){
+                System.exit(0);
+  }
+*/
