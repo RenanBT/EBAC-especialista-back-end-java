bfdd8fdcb6dbc5edb1bc1d07773cd960115ae3e3
Renan Betereli <renan@mantec.tech>
2023-04-18 19:47:44 -0300

    Commit exercicio reflections Modulo 19

--- Annotation/Annotation_Mod18/src/Main.java
+++ Annotation/Annotation_Mod18/src/Main.java
@@ -1,6 +1,6 @@
 // Criar uma annotation chamada "Tabela". Receberá um valor que vai ser o nome da tabela.
 
-
+// ler a annotation em tempo de execuçao imprimir nome da tabela no console
 public class Main {
     public static void main(String[] args) {
 
