17f5a91bf62e0cf1b2a5a067dbbe2f5ca714c6db
Renan Betereli <renan@mantec.tech>
2023-08-25 19:55:00 -0300

    Commit projeto utilizando Maven

---
+++ Maven_App/src/main/java/com/br/betereli/App.java
@@ -0,0 +1,13 @@
+package com.br.betereli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Primeiro Projeto usando Maven!" );
+    }
+}
